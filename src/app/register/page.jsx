196dea7fe932ecdee401aca828df885470e38cda
Lacamoura48 <anasfog@outlook.com>
2023-12-21 15:10:44 +0100

    asdf

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function page() {
+function Register() {
   return (
-    <div>page</div>
+    <div>Register</div>
   )
 }
 
-export default page
+export default Register
